0ec58942f591b891fcf9f0a6513adbed02f9a6a9
IsraelRAlencar <israelalencarribeiro@gmail.com>
2024-11-19 14:47:38 -0400

    new - creating and implementing LoansController

---
+++ src/LibraryService/Controllers/LoansController.cs
@@ -0,0 +1,97 @@
+using AutoMapper;
+using LibraryService.Data.Interfaces;
+using LibraryService.DTOs.LoanDTOs;
+using LibraryService.Entities;
+using MassTransit;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoansController : ControllerBase
+    {
+        private readonly ILoanRepository _repo;
+        private readonly IMapper _mapper;
+        private readonly IPublishEndpoint _publishEndpoint;
+
+        public LoansController(ILoanRepository repo, IMapper mapper, IPublishEndpoint publishEndpoint)
+        {
+            _repo = repo;
+            _mapper = mapper;
+            _publishEndpoint = publishEndpoint;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<LoanDto>>> GetAllLoans()
+        {
+            return await _repo.GetLoansAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<LoanDto>> GetLoanById(Guid id)
+        {
+            var loan = await _repo.GetLoanByIdAsync(id);
+            
+            if (loan == null) return NotFound();
+
+            return loan;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<LoanDto>> CreateLoan(CreateLoanDto createLoanDto)
+        {
+            var loan = _mapper.Map<Loan>(createLoanDto);
+            
+            _repo.AddLoan(loan);
+
+            var newLoan = _mapper.Map<LoanDto>(loan);
+
+            // await _publishEndpoint.Publish(_mapper.Map<LoanCreated>(newLoan));
+
+            var result = await _repo.SaveChangesAsync();
+
+            if (!result) return BadRequest("Error while creating loan");
+
+            return CreatedAtAction(nameof(GetLoanById), new { id = loan.Id }, _mapper.Map<LoanDto>(loan));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<LoanDto>> UpdateLoan(Guid id, UpdateLoanDto updateLoanDto)
+        {
+            var loan = await _repo.GetLoanByIdAsync(id);
+
+            if (loan == null) return NotFound();
+
+            loan.Observation = updateLoanDto.Observation ?? loan.Observation;
+            loan.ReturnDate = updateLoanDto.ReturnDate ?? loan.ReturnDate;
+
+            // await _publishEndpoint.Publish(_mapper.Map<LoanUpdated>(loan));
+
+            var result = await _repo.SaveChangesAsync();
+
+            if (!result) return BadRequest("Error while updating loan");
+
+            return _mapper.Map<LoanDto>(loan);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteLoan(Guid id)
+        {
+            var loan = await _repo.GetLoanEntityByIdAsync(id);
+
+            if (loan == null) return NotFound();
+
+            _repo.RemoveLoan(loan);
+
+            // await _publishEndpoint.Publish(_mapper.Map<LoanDeleted>(loan));
+
+            var result = await _repo.SaveChangesAsync();
+
+            if (!result) return BadRequest("Error while deleting loan");
+
+            return NoContent();
+        }
+    }
+}
--- src/LibraryService/DTOs/LoanDTOs/UpdateLoanDto.cs
+++ src/LibraryService/DTOs/LoanDTOs/UpdateLoanDto.cs
@@ -3,5 +3,5 @@ namespace LibraryService.DTOs.LoanDTOs;
 public class UpdateLoanDto
 {
     public string Observation { get; set; }
-    public DateTime ReturnDate { get; set; }
+    public DateTime? ReturnDate { get; set; }
 }
