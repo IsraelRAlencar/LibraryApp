1f3db0403b4182b66c2ec64a4ea37f1112e732c8
IsraelRAlencar <israelalencarribeiro@gmail.com>
2024-11-18 20:26:04 -0400

    new - setting up Program.cs

--- src/LibraryService/Program.cs
+++ src/LibraryService/Program.cs
@@ -1,8 +1,57 @@
+using LibraryService.Data;
+using Microsoft.EntityFrameworkCore;
+using MassTransit;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options => {
+        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+    });
+
+builder.Services.AddDbContext<LibraryDbContext>(opt => {
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("LibraryDb"));
+});
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddMassTransit(x => 
+{
+    x.AddEntityFrameworkOutbox<LibraryDbContext>(o => 
+    {
+        o.QueryDelay = TimeSpan.FromMilliseconds(100);
+
+        o.UsePostgres();
+        o.UseBusOutbox();
+    });
+
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("library-service", false));
+
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host(builder.Configuration["RabbitMq:Host"], "/", host =>
+        {
+            host.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
+            host.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
+        });
+
+        cfg.ConfigureEndpoints(context);
+    });
+});
 
 var app = builder.Build();
+
+app.UseAuthorization();
+app.MapControllers();
+
+// try
+// {
+//     DbInitializer.InitDb(app);
+// }
+// catch (Exception e)
+// {
+//     Console.WriteLine(e.Message);
+// }
 
 app.Run();
